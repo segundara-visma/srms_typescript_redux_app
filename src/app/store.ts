80ae05271c6561d927ad1182ce8ad0fee615105e
segundara-visma <olusegun.okedara@visma.com>
2023-02-11 13:41:24 +0200

    feat: added reducers into the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,32 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import loginReducer from '../features/login/loginSlice';
+import userReducer from '../features/user/userSlice';
+import studentReducer from '../features/student/studentSlice';
+import paginationReducer from '../features/pagination/paginationSlice';
+import tutorReducer from '../features/tutor/tutorSlice';
+import adminReducer from '../features/admin/adminSlice';
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // // Ignore these action types
+        // ignoredActions: ['your/action/type'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['meta.arg', 'payload.data'],
+        // Ignore these paths in the state
+        ignoredPaths: ['student.pdfData.data'],
+      },
+    }),
   reducer: {
     counter: counterReducer,
+    login: loginReducer,
+    user: userReducer,
+    student: studentReducer,
+    tutor: tutorReducer,
+    admin: adminReducer,
+    pagination: paginationReducer
   },
 });
 
