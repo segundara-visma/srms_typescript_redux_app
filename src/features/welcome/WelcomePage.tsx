4c23227140a5507bf2028f949276ba458f933199
segundara-visma <olusegun.okedara@visma.com>
2023-07-08 01:33:50 +0300

    fix: redirect to profile page if user is already logged-in

--- src/features/welcome/WelcomePage.tsx
+++ src/features/welcome/WelcomePage.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import "../../commonStyle/style.scss";
+import { Navigate } from "react-router-dom";
 
 const WelcomePage = () => {
+    const loggedIn = localStorage.getItem("userTitle")
     return (
-        <div className="jumbo text-center">
-            <p className="d-flex flex-column">
-                <span>Welcome to the School Portal.</span>
-                <span>Login details can be found in <a href="https://cloudy-blue-lion.cyclic.app/api-docs">api-docs</a></span>
-            </p>
-        </div>
+        <>
+            {loggedIn ? <Navigate to="/profile"/>
+                :(
+                    <div className="jumbo text-center">
+                        <p className="d-flex flex-column">
+                            <span>Welcome to the School Portal.</span>
+                            <span>Login details can be found in <a href="https://cloudy-blue-lion.cyclic.app/api-docs">api-docs</a></span>
+                        </p>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
