7be55228f59dba263feae54c2f63bfd742c2c274
segundara-visma <olusegun.okedara@visma.com>
2023-07-08 01:38:28 +0300

    refactor: included protected route for private pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import WelcomePage from './features/welcome/WelcomePage'
 import './App.css';
@@ -8,62 +8,36 @@ import Logout from './features/logout/Logout';
 import NavBar from './features/nav/NavBar';
 import AllCourses from './features/student/Courses';
 import MyCourses from './features/student/Registered';
-import SideNav from './features/nav/SideNav';
-import { Container, Row, Col } from "react-bootstrap";
-import Header from './features/user/PageHeader';
 import ExamsGrades from './features/student/ExamsGrades';
-import { useAppSelector } from '../src/app/hooks'
-import { selectValue } from './features/login/loginSlice';
 import StudentList from './features/tutor/MyStudentList';
 import ExamsGradesFromTutor from './features/tutor/ExamsGrades';
 import AdminStudentList from './features/admin/StudentList';
 import AdminTutorList from './features/admin/TutorList';
 import AdminCourseList from './features/admin/CourseList'
+import ProtectedRoute from './features/login/ProtectedRoute';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<string|null>()
-  const loggedIn = localStorage.getItem("userTitle");
-  const loggedInTitle = loggedIn && JSON.parse(loggedIn);
-  const user = useAppSelector(selectValue)
-
-  useEffect(() => {
-    setIsLoggedIn(loggedInTitle)
-  }, [loggedInTitle, user, isLoggedIn])
+  const loggedIn = localStorage.getItem("userTitle")
 
   return (
     <BrowserRouter>
-      <Container fluid style={{maxWidth: "100vw", overflowX: "hidden", maxHeight: "100vh", overflowY: "hidden"}} className="p-0">
-      <Row>
-        <Col><NavBar /></Col>
-      </Row>
-      <Row>
-        {isLoggedIn && <Col sm={2} className="border p-0 side-nav-container"><SideNav/></Col>}
-        <Col sm={isLoggedIn ? 10 : 12}>
-          {isLoggedIn && <Row>
-            <Col><Header/></Col>
-          </Row>}
-          
-          <Row>
-            <Col style={{backgroundColor: "#f8f9fa"}} className="border-top">
-              <Routes>
-                <Route path="/" element={<WelcomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/courses" element={<AllCourses />} />
-                <Route path="/my-courses" element={<MyCourses />} />
-                <Route path="/exams" element={<ExamsGrades />} />
-                <Route path="/my-students" element={<StudentList />} />
-                <Route path="/exams-grades" element={<ExamsGradesFromTutor />} />
-                <Route path="/students-list" element={<AdminStudentList />} />
-                <Route path="/tutors-list" element={<AdminTutorList />} />
-                <Route path="/course-list" element={<AdminCourseList />} />
-              </Routes>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      </Container>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProtectedRoute isAuthenticated={!!loggedIn}/>}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/courses" element={<AllCourses />} />
+          <Route path="/my-courses" element={<MyCourses />} />
+          <Route path="/exams" element={<ExamsGrades />} />
+          <Route path="/my-students" element={<StudentList />} />
+          <Route path="/exams-grades" element={<ExamsGradesFromTutor />} />
+          <Route path="/students-list" element={<AdminStudentList />} />
+          <Route path="/tutors-list" element={<AdminTutorList />} />
+          <Route path="/course-list" element={<AdminCourseList />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
